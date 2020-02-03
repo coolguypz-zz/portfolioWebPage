f7afc81eb9b35a5ca74e7930422ad092fe57d4bf
Peterzhang123 <peterzzq1986@gmail.com>
2020-02-03 10:43:11 -0800

    update page title

--- client/componets/nav/navData.js
+++ client/componets/nav/navData.js
@@ -3,31 +3,31 @@ const navData = [
   {
     className:`nav-item`,
     aTagClassName:`nav-link`,
-    href:`#page1`,
+    href:`#home`,
     text:`Home`,
   },
   {
     className:`nav-item`,
     aTagClassName:`nav-link`,
-    href:`#page2`,
+    href:`#myproject`,
     text:`My project`,
   },
   {
     className:`nav-item`,
     aTagClassName:`nav-link`,
-    href:`#page3`,
+    href:`#coverletter`,
     text:`introduce`,
   },
   {
     className:`nav-item`,
     aTagClassName:`nav-link`,
-    href:`#page4`,
+    href:`#skills`,
     text:`My skills`,
   },
   {
     className:`nav-item`,
     aTagClassName:`nav-link`,
-    href:`#page5`,
+    href:`#contact`,
     text:`contact me`,
   },
   // {
@@ -43,28 +43,4 @@ const navData = [
   //   text:`Home`,
   // },
 ]
-
-
-
-// <li class="nav-item active">
-// <a class="nav-link" href="#page1">Home <span class="sr-only">(current)</span></a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link" href="#page2">My project</a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link" href="#page3">introduce</a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link" href="#page4">My skills</a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link" href="#page5">contact me</a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link" href="http://sweet-corner.zhiqinz.com/">Sweet Corner</a>
-// </li>
-// <li class="nav-item">
-// <a class="nav-link" href="http://sgt.zhiqinz.com/">Student Grade</a>
-// </li>
 
--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const path = require("path");
 
 const app = express();
 
-// app.use(express.urlencoded({extends:false}));
-
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname,'client')))
-
-// app.get('*',(req,res)=>{
-//   res.sendFile(path.resolve(__dirname,'client','index.html'))
-// })
 
 app.listen(port,()=> console.log("listening on port:",port));
